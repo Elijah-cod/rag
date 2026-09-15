e29cfa9660cba730520103ef4112d2a644b146dd
Elijah Mathai <iamngunjiri@gmail.com>
2026-09-15 13:54:24 +0300

    Add loader function

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -7,6 +7,31 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# Load the txt documents
+def load_documents(docs_path="docs"):
+    print(f"Loading documents from {docs_path}...")
+
+    # Check if docs directory exists
+    if not os.path.exists(docs_path):
+        raise FileNotFoundError(f"The directory '{docs_path}' does not exist.")
+
+    # Load all txt files from the  docs directory
+    loader = DirectoryLoader(docs_path, glob="*.txt", loader_cls=TextLoader)
+    documents = loader.load()
+
+    if len(documents) == 0:
+        raise ValueError(f"No .txt found in the directory '{docs_path}'.")
+
+    # Show the first 2 documents 
+    for i, doc in enumerate(documents[:2]):
+        print(f"\nDocument {i + 1}:")
+        print(f" Source: {doc.metadata['source']}")
+        print(f" Content length: {len(doc.page_content)} characters")
+        print(f" Content preview: {doc.page_content[:200]}...")  # Print first 200 characters
+        print(f" Metadata: {doc.metadata}")
+    
+    return documents
+
 def main():
     print("Main function")
 
