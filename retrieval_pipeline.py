b33a647c159bd2d6e9ed7c6a855d2f986a989b05
Elijah Mathai <iamngunjiri@gmail.com>
2026-09-16 12:41:44 +0300

    Add retrieval pipeline

---
+++ retrieval_pipeline.py
@@ -0,0 +1,29 @@
+from langchain_chroma import Chroma
+from langchain_openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+# Access the environment variables from the .env file
+load_dotenv()
+
+# Load the storage directory
+persist_directory = "db/chroma_db"
+
+# Load embeddings
+embedding_model = OpenAIEmbeddings(model="text-embedding-3-small")
+
+# Initialize the Chroma vector store
+db = Chroma(persist_directory=persist_directory, embedding_function=embedding_model, collection_metadata={"hnsw:space": "cosine"})
+
+query = "Which island does SpaceX lease for its launches in the pacific?"
+
+retriever = db.as_retriever(
+    search_kwargs={"k": 10}
+)
+
+relevant_docs = retriever.invoke(query)
+
+print("--- Context ---")
+
+for i, doc in enumerate(relevant_docs, 1):
+    print(f"\nDocument {i}:")
+    print(doc.page_content)
+    print(f"Source: {doc.metadata.get('source')}")
